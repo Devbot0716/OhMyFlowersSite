bcd6e039a4422c9c2af85400d83388c6cbd45be6
Devbot0716 <pzepher@gmail.com>
2017-06-20 12:35:57 -0600

    I did it

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+angular.module('app', ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home')//sends customer to this page as the default
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: './views/home.html'
+      })
+
+      .state('prices', {
+        url: '/prices',
+        templateUrl: './views/prices.html'
+      })
+
+      .state('contacts', {
+        url: '/contacts',
+        templateUrl: './views/contacts.html'
+      })
+
+      .state('about', {
+        url: '/about',
+        templateUrl: './views/about.html'
+      })
+
+      .state('boutonniere', {
+        url: '/boutonniere',
+        templateUrl: './views/boutonniere.html'
+      })
+
+      .state('bridalBouquet', {
+        url: '/bridalBouquet',
+        templateUrl: './views/bridalBouquet.html'
+      })
+
+      .state('corsage', {
+        url: '/corsage',
+        templateUrl: './views/corsage.html'
+      })
+
+      .state('funeral', {
+        url: '/funeral',
+        templateUrl: './views/funeral.html'
+      })
+
+  });
